3f0d7aa22b3a44fd9eb6fcb7bea886e2e11a9cfa
cmrib <ciceromirandar@gmail.com>
2022-07-18 18:39:39 -0300

    install svg transformer

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,8 @@
 import { VStack, Heading } from 'native-base';
 import React from 'react';
+import Logo from '../assets/logo_primary.svg'
+
+
 
 export function SignIn() {
     return (
@@ -7,6 +10,7 @@ export function SignIn() {
             <Heading color="gray.100" fontSize="xl" mt={20} mb="6">
                 Acesse a sua conta
             </Heading>
+            <Logo width={120} height={40} />
 
         </VStack>
     );
