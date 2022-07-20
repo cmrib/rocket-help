f21f878a1330f6d1e3b1070201e1d635b2a031be
cmrib <ciceromirandar@gmail.com>
2022-07-19 23:01:16 -0300

    create register screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import React from 'react';
-import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Register /> : <Loading />}
     </NativeBaseProvider>
   );
 }
